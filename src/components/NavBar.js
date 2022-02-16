c8b738af2b4af76f6cc28c2f0b2be952df8caff4
Your Name <you@example.com>
2022-02-16 22:17:31 +0600

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+//image
+import backArrow from '../images/icons/backArrow.svg'
+/*Nav style  */
+import './NavBar.css'
+
+export default function NavBar() {
+  return (
+    <>
+        <div className='Nav_container'>
+            <nav>
+                <Link to="/" className='Back_btn'> <img src={backArrow}/> <span className='nav_back'>Back</span> </Link>
+                <h2 className='nav_heading'>Connect Wallet</h2>
+                <Link to="#" className='home_btn'>Home</Link>
+            </nav>
+        </div>
+    </>
+  )
+}
---
+++ src/views/connect walets/ConnectWalet.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import NavBar from '../../components/NavBar'
+import walletIcon from '../../images/icons/wallet.svg'
+import metaMask from '../../images/icons/metamask.svg'
+import trustWallet from '../../images/icons/trustWallet.svg'
+import { Link } from 'react-router-dom'
+
+
+
+/* style css */
+import './connectWalet.css'
+
+export default function ConnectWalet() {
+    return (
+        <>
+            <NavBar />
+            {/* wallet icon and info */}
+            <div className='wallet_icon_container'>
+                <img src={walletIcon} />
+                <div className='wallet_info'>
+                    <p>Lorem Ipsum is simply dummy text of the
+                        printing and typesetting industry. Lorem
+                        Ipsum has been the industry's
+                        standard dummy text ever since the</p>
+                </div>
+            </div>
+
+            {/* wallets available to connect */}
+            <div className='available_wallet'>
+                <div className='single_wallet_container'>
+                    <Link to='/' className='wallet_container'>
+                        <img src={metaMask}/>
+                        <span className='wallet_text'>MetaMask</span>
+                    </Link>
+                </div>
+
+                <div className='single_wallet_container'>
+                    <Link to='/' className='wallet_container'>
+                        <img src={trustWallet}/>
+                        <span className='wallet_text'>Trust Wallet</span>
+                    </Link>
+                </div>
+                
+            </div>
+            <div className='new_wallet_user_more'>
+                <h3 className='new_wallet_creat'>New to Wallets ?</h3>
+                <Link to='/' className='learn_more'>Learn more</Link>
+
+            </div>
+        </>
+    )
+}
