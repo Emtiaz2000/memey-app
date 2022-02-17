fc68db56896e97f23011cc0c6562ddf20ae84b6a
Your Name <you@example.com>
2022-02-18 02:00:35 +0600

    adjusting the height of editor option

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
     <h1>Home</h1>
-    <Link to='/connect-wallet'> go connect wallet page</Link>
+    <Link to='/connect-wallet'> go connect wallet page</Link><br/>
     <Link to='/generate-meme'>Go to meme generator</Link>
     </>
   )
